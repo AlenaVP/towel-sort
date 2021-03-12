be9c0fa567cd5d11d69d3a46cfa490890c24af68
AlenaVP <poprugo@gmail.com>
2021-03-12 16:37:39 +0300

    fix: add an input data validation

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return matrix.map(l => matrix.indexOf(l) % 2 === 0 ? l : l.reverse()).join(',').split(',');
+  return (matrix === undefined) || (matrix.length === 0) ? [] : (matrix.map(l => matrix.indexOf(l) % 2 === 0 ? l : l.reverse()).join(',').split(','));
 }
